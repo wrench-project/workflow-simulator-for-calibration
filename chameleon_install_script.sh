ec55e6c4534fc06fde3bb61b1aea5c15244a2dd5
Henri Casanova <henric@hawaii.edu>
2023-02-07 20:18:09 -1000

    chameleon install script  update

--- chameleon_install_script.sh
+++ chameleon_install_script.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 
 # Install python 3.9
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install -y python3.9
 sudo update-alternatives --install /usr/local/bin/python3 python3 /usr/bin/python3.9 10
@@ -22,19 +22,21 @@ git clone https://github.com/wfcommons/wfcommons.git && cd wfcommons && git chec
 # Install DeepHyper
 python3 -m pip install -r calibration/requirements.txt
 
+
 # Install Docker
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo newgrp docker
+
+# Pull Docker container
+docker pull wrenchproject/workflow-calibration
 
 # Done
+echo "***"
 echo "You should now be able to run: ./calibration/calibrate.py -c ./calibration/config.json"
 
 
