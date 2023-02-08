c0a13cd0e4774bb4181103d1a08b36b81477436d
Henri Casanova <henric@hawaii.edu>
2023-02-07 21:12:28 -1000

    Updated Chameleon install script

--- chameleon_install_script.sh
+++ chameleon_install_script.sh
@@ -26,11 +26,18 @@ python3 -m pip install -r calibration/requirements.txt
 # Install Docker
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
+sudo /bin/rm -f /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
+sudo groupadd --force docker
+sudo usermod -aG docker `whoami`
+if id -nG `whoami` | grep -qw "docker"; then
+    echo "docker group is already active"
+else
+    newgrp docker
+fi
 
 # Pull Docker container
 docker pull wrenchproject/workflow-calibration
