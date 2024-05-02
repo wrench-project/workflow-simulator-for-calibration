64ccc8c317c4e5c6c205e664b240e5752487add5
Henri Casanova <henric@hawaii.edu>
2024-05-01 15:21:28 -1000

    Script fix

--- simcal/run_all_single_workflow_experiments.sh
+++ simcal/run_all_single_workflow_experiments.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <time limit in seconds> <number of threads to use>"
+    exit 1
+fi
+
+TIME_LIMIT="$1"
+NUM_THREADS="$2"
 WORKFLOWS="chain cycles epigenomics forkjoin genome montage seismology soykb"
 ARCHITECTURES="haswell skylake cascadelake"
 
 export DYLD_LIBRARY_PATH=/usr/local/lib
 
-TIME_LIMIT=10
-NUM_THREADS=8
 
 for workflow in $WORKFLOWS; do
         for architecture in $ARCHITECTURES; do
-		python3 ./run_single_workflow_experiments.py -wd ../../JSONS -wn $workflow -ar $architecture -al random -tl $TIME_LIMIT -th $NUM_THREADS -lf relative_average_error -cs htcondor_bare_metal -ss submit_and_compute_hosts  -ns one_and_then_many_links -cn CCskylake 
+            echo "* Experiment: $workflow on $architecture"
+	        	python3 ./run_single_workflow_experiments.py -wd ../../JSONS -wn $workflow -ar $architecture -al random -tl $TIME_LIMIT -th $NUM_THREADS -lf relative_average_error -cs htcondor_bare_metal -ss submit_and_compute_hosts  -ns one_and_then_many_links -cn CCskylake
         done 
 done
 
