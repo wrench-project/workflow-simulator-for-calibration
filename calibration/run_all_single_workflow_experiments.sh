4e1947191ae9a4dd3e60d141b1203ed8b63ded0b
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-07-24 14:29:21 -1000

    updated script

--- calibration/run_all_single_workflow_experiments.sh
+++ calibration/run_all_single_workflow_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <grid | random | gradient> <time limit in seconds> <number of threads to use>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <grid | random | gradient> <time limit in seconds> <number of threads to use> <workflows> <architectures> <locale>"
     exit 1
 fi
 
@@ -9,18 +9,22 @@ ALGORITHM="$1"
 TIME_LIMIT="$2"
 NUM_THREADS="$3"
 
-WORKFLOWS="chain cycles epigenomics forkjoin genome montage seismology soykb"
+WORKFLOWS = "$4"
+#WORKFLOWS="chain cycles epigenomics forkjoin genome montage seismology soykb"
 #WORKFLOWS="bwa"
-ARCHITECTURES="haswell skylake cascadelake"
+
+ARCHITECTURES = "$5"
+#ARCHITECTURES="haswell skylake cascadelake"
 #ARCHITECTURES="cascadelake"
 
+LOCALE="$6"
 export DYLD_LIBRARY_PATH=/usr/local/lib
 
 
 for workflow in $WORKFLOWS; do
         for architecture in $ARCHITECTURES; do
             echo "* Experiment: $workflow on $architecture"
-	        	echo python3 ./run_single_workflow_experiments.py -wd ../../JSONS -wn $workflow -ar $architecture -al $ALGORITHM -tl $TIME_LIMIT -th $NUM_THREADS -lf relative_average_error -cs htcondor_bare_metal -ss submit_and_compute_hosts  -ns one_and_then_many_links -cn CCskylake
+	        	python3 ./run_single_workflow_experiments.py -wd ../../JSONS -wn $workflow -ar $architecture -al $ALGORITHM -tl $TIME_LIMIT -th $NUM_THREADS -lf relative_average_error -cs htcondor_bare_metal -ss submit_and_compute_hosts  -ns one_and_then_many_links -cn $LOCALE
         done 
 done
 
