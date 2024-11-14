ad6bbca975019bd37855a52d49180cef0cbeeede
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-11-13 16:02:45 -1000

    Update reevaluate_pickle.py

--- calibration/reevaluate_pickle.py
+++ calibration/reevaluate_pickle.py
@@ -8,6 +8,13 @@ from Util import *
 
 
 if __name__ == "__main__":
+	if len(sys.args<2):
+		print(f"Usage:{sys.args[0]} pickle_file") 
+		exit()
+	if len(sys.args>2):
+		print(f"{sys.args[0]} * not supported, try find") 
+		exit()
+	pickle_path=sys.args[1]
 	with open(pickle_path, 'rb') as f:
 		data = pickle.load(f)
 	data.compute_all_evaluations()
